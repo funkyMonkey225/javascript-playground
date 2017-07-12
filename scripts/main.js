b4138e32bd31c94705565bd2849cb55dd1785d70
Mac User <sabbey37@gmail.com>
2017-07-12 13:11:33 -0400

    Add tipAmount function

--- scripts/main.js
+++ scripts/main.js
@@ -10,3 +10,15 @@ function hello(name) {
 function madlib(name, subject) {
     return name + "'s favorite subject in school is " + subject + ".";
 }
+
+function tipAmount(bill, service) {
+    if (service == "good") {
+        return bill * .20;
+    } else if (service == "fair") {
+        return bill * .15;
+    } else if (service == "bad") {
+        return bill * .10;
+    } else {
+        return undefined;
+    }
+}
