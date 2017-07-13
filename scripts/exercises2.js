c054b0c95378f35b31eef1b7dfeeeceabbb8fb16
Mac User <sabbey37@gmail.com>
2017-07-13 13:54:35 -0400

    Add another method of  longLongVowels function

--- scripts/exercises2.js
+++ scripts/exercises2.js
@@ -97,58 +97,75 @@ function leetSpeak(text) {
 
 function longLongVowels(text) {
     var longVow = text.toUpperCase();
+    var vowels = {"A": "AAAA", "E": "EEEE", "I":"IIII", "O":"OOOO", "U":"UUUU"};
     var savedvowel = "";
     var s = "";
     var i = 0;
     while (i < longVow.length) {              
-        var vowel = longVow[i];          
-        if (vowel === "A") {               
+        var vowel = longVow[i];
+        if (vowel in vowels) {
             if (savedvowel === vowel) {
-                s = s + vowel.repeat(4);        
+                s = s + vowels[vowel];        
             } else {                      
                 s = s + vowel;
             }           
-            savedvowel = vowel;     
-            
-        } else if (vowel === "E") {
-            if (savedvowel === vowel) {
-                s = s + vowel.repeat(4);
-            } else {
-                s = s + vowel;
-            }
             savedvowel = vowel;
-
-        } else if (vowel === "I") {
-            if (savedvowel === vowel) {
-                s = s + vowel.repeat(4);  
-            } else {
-                s = s + vowel;
-            }
-            savedvowel = vowel;
-
-        } else if (vowel === "O") {
-            if (savedvowel === vowel) {
-                s = s + vowel.repeat(4);
-            } else {
-                s = s + vowel;
-            }
-            savedvowel = vowel;
-
-        } else if (vowel === "U") {
-            if (savedvowel === vowel) {
-                s = s + vowel.repeat(4);
-            } else {
-                s = s + vowel;
-            }
-            savedvowel = vowel;
-        } else {                       
+        } else {
             s = s + vowel;
             savedvowel = "";
         }
         i += 1;
     }
-    console.log(s);          
+    return s; 
 }
+        
+    //     if (vowel === "A") {               
+    //         if (savedvowel === vowel) {
+    //             s = s + vowel.repeat(4);        
+    //         } else {                      
+    //             s = s + vowel;
+    //         }           
+    //         savedvowel = vowel;     
+            
+    //     } else if (vowel === "E") {
+    //         if (savedvowel === vowel) {
+    //             s = s + vowel.repeat(4);
+    //         } else {
+    //             s = s + vowel;
+    //         }
+    //         savedvowel = vowel;
+
+    //     } else if (vowel === "I") {
+    //         if (savedvowel === vowel) {
+    //             s = s + vowel.repeat(4);  
+    //         } else {
+    //             s = s + vowel;
+    //         }
+    //         savedvowel = vowel;
+
+    //     } else if (vowel === "O") {
+    //         if (savedvowel === vowel) {
+    //             s = s + vowel.repeat(4);
+    //         } else {
+    //             s = s + vowel;
+    //         }
+    //         savedvowel = vowel;
+
+    //     } else if (vowel === "U") {
+    //         if (savedvowel === vowel) {
+    //             s = s + vowel.repeat(4);
+    //         } else {
+    //             s = s + vowel;
+    //         }
+    //         savedvowel = vowel;
+    //     } else {                       
+    //         s = s + vowel;
+    //         savedvowel = "";
+    //     }
+    //     i += 1;
+    // }
+    // console.log(s);          
+
 
 function sumNumbers(numbers) {
     var i = 0;
@@ -211,77 +228,101 @@ function matrixMultiply(matrix1, matrix2) {
     return newMatrix;
 }
 
-function rockPaperScissors(player1, player2) {
-    player1.toLowerCase();
-    player2.toLowerCase();
-   
-    if (player1 === player2) {
-        return "draw";
-    } else if (player1 === "rock" && player2 === "paper") {
-        return "player 2";
-    } else if (player1 === "rock" && player2 === "scissors") {
-        return "player 1";
-    } else if (player1 === "paper" && player2 === "rock") {
-        return "player 1";
-    } else if (player1 === "paper" && player2 === "scissors") {
-        return "player 2";
-    } else if (player1 === "scissors" && player2 === "rock") {
-        return "player 2";
-    } else if (player1 === "scissors" && player2 === "paper") {
-        return "player 1";
+function playAgain(input) {
+    input = prompt("Would you like to play again? ('yes' or 'no')");
+    input = input.toLowerCase();
+    if (input === "yes") {
+        rockPaperScissors();
+    } else if (input === "no") {
+        return console.log("Goodbye!");
+    } else {
+        console.log("Invalid input.");
+        playAgain();
     }
 }
 
+function rockPaperScissors() {
+    var player1 = prompt("Player 1's move: ");
+    var player2 = prompt("Player 2's move: ");
+    var result = "";
+    player1 = player1.toLowerCase();
+    player2 = player2.toLowerCase();
+    
+    if (player1 === player2) {
+        result = "Draw";
+    } else if (player1 === "rock" && player2 === "paper") {
+        result = "Player 2 wins!";
+    } else if (player1 === "rock" && player2 === "scissors") {
+        result = "Player 1 wins!";
+    } else if (player1 === "paper" && player2 === "rock") {
+        result = "Player 1 wins!";
+    } else if (player1 === "paper" && player2 === "scissors") {
+        result = "Player 2 wins!";
+    } else if (player1 === "scissors" && player2 === "rock") {
+        result = "Player 2 wins!";
+    } else if (player1 === "scissors" && player2 === "paper") {
+        result = "Player 1 wins!";
+    } else {
+        console.log("Invalid input. Please choose one: 'rock', 'paper', or 'scissors'");
+        rockPaperScissors();
+    }
+    console.log(result);
+    playAgain();
+}
+
+
 function ticTacToe(userArray) {
+    var result = "";
     if (userArray[0][0] === userArray[0][1] && userArray[0][1] === userArray[0][2]) {
         if (!null) {
-            return userArray[0][0] + " wins!";
+            result = userArray[0][0] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else if (userArray[1][0] === userArray[1][1] && userArray[1][1] === userArray[1][2]) {
         if (!null) {
-            return userArray[1][0] + " wins!";
+            result = userArray[1][0] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else if (userArray[2][0] === userArray[2][1] && userArray[2][1] === userArray[2][2]) {
         if (!null) {
-            return userArray[1][0] + " wins!";
+            result = userArray[1][0] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else if (userArray[0][0] === userArray[1][0] && userArray[1][0] === userArray[2][0]) {
         if (!null) {
-            return userArray[0][0] + " wins!";
+            result = userArray[0][0] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else if (userArray[0][1] === userArray[1][1] && userArray[1][1] === userArray[2][1]) {
          if (!null) {
-            return userArray[0][1] + " wins!";
+            result = userArray[0][1] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else if (userArray[0][2] === userArray[1][2] && userArray[1][2] === userArray[2][2]) {
          if (!null) {
-            return userArray[0][2] + " wins!";
+            result = userArray[0][2] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else if (userArray[0][0] === userArray[1][1] && userArray[1][1] === userArray[2][2]) {
         if (!null) {
-            return userArray[0][2] + " wins!";
+            result = userArray[0][2] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else if (userArray[0][2] === userArray[1][1] && userArray[1][1] === userArray[2][0]) {
         if (!null) {
-            return userArray[0][2] + " wins!";
+            result = userArray[0][2] + " wins!";
         } else {
-            return null;
+            result = null;
         }
     } else {
-        return null;
+        result = null;
     }
+    return result;
 }
