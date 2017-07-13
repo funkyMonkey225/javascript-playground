8793dcd32ffa638a77084217615ec69277d87c7b
Mac User <sabbey37@gmail.com>
2017-07-13 11:47:47 -0400

    new tipAmount function example using dictionary

--- scripts/main.js
+++ scripts/main.js
@@ -12,15 +12,41 @@ function madlib(name, subject) {
 }
 
 function tipAmount(bill, service) {
+    var tip = 0;
     if (service == "good") {
-        return bill * .20;
+        tip = bill * .20;
     } else if (service == "fair") {
-        return bill * .15;
+        tip = bill * .15;
     } else if (service == "bad") {
-        return bill * .10;
+        tip = bill * .10;
     } else {
-        return undefined;
+        tip = undefined;
     }
+    return tip;
+}
+
+
+function tipAmount2(bill, serviceLevel) {
+    var tip = 0;
+    // if (!serviceLevel) {
+    //     serviceLevel = good;
+    // }
+    // serviceLevel = serviceLevel || good;
+
+    if (!serviceLevel) {
+        throw new Error("Value Error: please pass in string as serviceLevel");
+    }
+
+    var serviceLevels = {
+        "good": 0.2,
+        "fair": 0.15,
+        "bad":  0.1,
+        "poor": 0.05
+    }
+    if (serviceLevel in serviceLevels) {
+        tip = bill * serviceLevels[serviceLevel];
+    }
+    return tip;
 }
 
 function totalAmount(bill, service) {
