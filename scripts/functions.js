f3c5c27d2683e73ec3462dfb6af845421457db6f
Mac User <sabbey37@gmail.com>
2017-07-14 14:27:26 -0400

    Add cityNames function

--- scripts/functions.js
+++ scripts/functions.js
@@ -17,3 +17,48 @@ function justEvens(array) {
     return justEven;
 }
 
+function squared(num) {
+    return num * num;
+}
+
+// Returns only squared numbers in a given array
+function squaredArray(array) {
+    var result = array.map(squared);
+    return result;
+}
+
+var cities = [
+    { name: 'Beaumont', temperature: 97.0 },
+    { name: 'Atlanta', temperature: 91.0 },
+    { name: 'Madison', temperature: 68.0 },
+    { name: 'Anchorage', temperature: 60.0 },
+    { name: 'Seattle', temperature: 68.0 },
+    { name: 'San Diego', temperature: 80.0 }
+];
+
+function tempCheck(obj) {
+    return  obj.temperature < 70.0;
+}
+
+function coolCities(array) {
+    var result = array.filter(tempCheck);
+    return result;
+}
+
+function getCityNames(obj) {
+    return obj.name;
+}
+
+function cityNames(array) {
+    var result = array.map(getCityNames);
+    return result;
+}
+
+
+// for (var i = 0; i < cities.length; i++) {
+//     if (cities[i][temperature] < 70.0) {
+//         coolCities.push(cities[i][name]);
+//     }
+//     return coolCities;
+// }
+  
